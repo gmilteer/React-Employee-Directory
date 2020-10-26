18091ea113d5736989b50f8aa307c73c8d14a57c
Georgia Milteer <gmilteer@gmail.com>
2020-10-26 19:54:53 -0400

    second push

---
+++ employeedirectory/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header/Index";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
---
+++ employeedirectory/src/components/Header/Index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <p>
+        Click on the heading to filter through or use the search box to narrow
+        your search results
+      </p>
+    </div>
+  );
+}
+
+export default Header;
