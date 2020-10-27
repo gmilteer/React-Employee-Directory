2358e6ec595310bcae4389cf5f3f3bdf661d90a1
Georgia Milteer <gmilteer@gmail.com>
2020-10-26 21:29:23 -0400

    Updates

---
+++ precache-manifest.c2e5d36660780e2f7a92db268b31e0d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3167a52d1a66852b0a94ac267fa99adb",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "bc989c507a9042ab17c1",
+    "url": "/React-Employee-Directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "9bd68ac8c9aa48362af8",
+    "url": "/React-Employee-Directory/static/css/main.8572bfc8.chunk.css"
+  },
+  {
+    "revision": "bc989c507a9042ab17c1",
+    "url": "/React-Employee-Directory/static/js/2.53d41659.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/React-Employee-Directory/static/js/2.53d41659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd68ac8c9aa48362af8",
+    "url": "/React-Employee-Directory/static/js/main.b4d6d945.chunk.js"
+  },
+  {
+    "revision": "fd74d32e2fdcce7b26d8",
+    "url": "/React-Employee-Directory/static/js/runtime-main.021b6c8f.js"
+  }
+]);
